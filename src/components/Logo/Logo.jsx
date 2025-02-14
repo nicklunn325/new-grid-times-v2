e04b7cb7af3f07ee0e8ef75d57f23265fbec00ae
Nick <nrlunn325@gmail.com>
2025-02-14 06:47:38 -0600

    solution

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import styled from 'styled-components';
 import format from 'date-fns/format';
 
+import { QUERIES } from '../../constants';
+
 const Logo = (props) => {
-  return (
-    <Wrapper>
-      <Link href="/" {...props}>
-        New Grid Times
-      </Link>
-      <TodaysDate>
-        {format(new Date(), 'EEEE, MMMM do, yyyy')}
-      </TodaysDate>
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <Link href="/" {...props}>
+                New Grid Times
+            </Link>
+            <TodaysDate>
+                {format(new Date(), 'EEEE, MMMM do, yyyy')}
+            </TodaysDate>
+        </Wrapper>
+    );
 };
 
 const Wrapper = styled.div`
@@ -22,6 +24,10 @@ const Wrapper = styled.div`
 const Link = styled.a`
   font-family: var(--font-family-logo);
   font-size: 3rem;
+
+  @media ${QUERIES.tabletAndUp} {
+    font-size: 4rem;
+  }
 `;
 
 const TodaysDate = styled.p`
--- src/components/OpinionStory/OpinionStory.jsx
+++ src/components/OpinionStory/OpinionStory.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { QUERIES } from '../../constants';
+
 const OpinionStory = ({ id, title, author, avatar }) => {
-  return (
-    <a href={`/story/${id}`}>
-      <Wrapper>
-        <Avatar alt="" src={avatar} />
-        <div>
-          <AuthorName>{author}</AuthorName>
-          <ArticleTitle>{title}</ArticleTitle>
-        </div>
-      </Wrapper>
-    </a>
-  );
+    return (
+        <a href={`/story/${id}`}>
+            <Wrapper>
+                <Avatar alt="" src={avatar} />
+                <div>
+                    <AuthorName>{author}</AuthorName>
+                    <ArticleTitle>{title}</ArticleTitle>
+                </div>
+            </Wrapper>
+        </a>
+    );
 };
 
 const Wrapper = styled.article`
@@ -25,6 +27,13 @@ const Avatar = styled.img`
   height: 48px;
   border-radius: 50%;
   object-fit: cover;
+  float: right;
+  margin-left: 16px;
+
+  @media ${QUERIES.tabletOnly} {
+    float: revert;
+    margin-left: revert;
+  }
 `;
 
 const AuthorName = styled.p`
